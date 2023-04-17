f5fb6694ffd62f95a63d337ac28d775ffab91f97
jetfire679 <jetfire679@gmail.com>
2023-04-17 09:34:56 -0400

    syntax fix

--- 0-variables.tf
+++ 0-variables.tf
@@ -1,11 +1,11 @@
 variable "cluster_name" {
     description = "EKS Cluster Name"
-    type = "string"
+    type = string
     default = "EKS"
 }
 
 variable "cluster_version" {
     description = "EKS Cluster Version"
-    type = "string"
+    type = string
     default = "1.22"
 }
